3efe05eb268fc35033ea7bc53e35dee8812e0ad0
THERISINGSUN <36433536+surajpanker@users.noreply.github.com>
2022-12-18 15:04:50 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import './App.css';
+import Loader from './Loader';
+import React from 'react';
+
+const {  useEffect } = React;
 
 function App() {
+
+  useEffect(() => {
+setTimeout(() => {
+      <Loader/>
+
+    }, 5000);
+
+  }, []);
+
+
   return (
     <div className="App">
+     
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+        <img src="ram.gif" className="App-logo Loader" alt="logo" />
+       
+       
+        <p >
+        भरी दुपहरी में अंधियारा,<br></br>
+        सूरज परछाईं से हारा,<br></br>
+        अंतरतम का नेह निचोड़ें, <br></br>
+        बुझी हुई बाती सुलगाएं।<br></br>
+        आओ फिर से दिया जलाएं।<br></br>
+        हम पड़ाव को समझे मंजिल,<br></br>
+        लक्ष्य हुआ आंखों से ओझल,<br></br>
+        वर्तमान के मोहजाल में आने वाला कल न भुलाएंआओ फिर से दिया जलाएं।<br></br>
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <h6 >
+          Created By  <span className="heart">♥️</span> 
+             Suraj Panker
+        </h6>
+       
+
       </header>
     </div>
   );
---
+++ src/Loader.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="giphy.gif"  alt="Loader" />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
